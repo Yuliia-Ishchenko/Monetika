aaaf449263d05250db512f4d9f38abeab0910f80
Yuliia Ishchenko <golovinadic@gmail.com>
2025-01-19 19:28:22 +0100

    ivan

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-// const supabaseUrl = 'https://hrrixouqeulmxuhssneb.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhycml4b3VxZXVsbXh1aHNzbmViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyMDc3MTgsImV4cCI6MjA1Mjc4MzcxOH0.fHMR3QJE4av5n33L-hOiz7v03ZN5opRVDh0wLOd9H-8';
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://hrrixouqeulmxuhssneb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhycml4b3VxZXVsbXh1aHNzbmViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyMDc3MTgsImV4cCI6MjA1Mjc4MzcxOH0.fHMR3QJE4av5n33L-hOiz7v03ZN5opRVDh0wLOd9H-8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's PORT environment variable or fallback to 3000
+    host: '0.0.0.0', // Ensure that the app is accessible externally on Render
+  },
+  build: {
+    outDir: 'dist', // Directory for production files
+    chunkSizeWarningLimit: 500, // This is to handle any chunk size warnings, adjust as necessary
+  },
 })
